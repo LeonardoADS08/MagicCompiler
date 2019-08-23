15d721fdf255078ed2d948bb219e5040e5bac079
Leonardo Arteaga <LeonardoADS08@hotmail.com>
2019-08-23 17:08:24 -0400

    Primer commit, ForgeAPI para maquinas de estados, enumeradores en tiempo de ejecución y Logs de ejecución.

---
+++ Forge API/FSM/State.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForgeAPI.FSM
+{
+    /// <summary>
+    /// State with no extra functionality used for root of state hierarchy.
+    /// </summary>
+    public class State : AbstractState { }
+}
