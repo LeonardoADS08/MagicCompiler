3a103f9aa8f32607522d7b7e26644ce39656d92d
Leonardo Arteaga <LeonardoADS08@hotmail.com>
2019-09-25 21:42:47 -0400

    Arreglando pequeños errores con los saltos de lineas y espacios.

--- Magic Compiler Core/LexicalAnalyzer/Lexer.cs
+++ Magic Compiler Core/LexicalAnalyzer/Lexer.cs
@@ -20,7 +20,6 @@ namespace MagicCompiler.LexicalAnalyzer
             {
                 _input = reader.ReadToEnd();
             }
-            _input.Replace(Environment.NewLine, " ");
         }
 
         public void Tokenize()
@@ -30,23 +29,17 @@ namespace MagicCompiler.LexicalAnalyzer
             for (int i = 0; i < _input.Length; i++)
             {
                 word += _input[i];
-                word = word.Trim();
-                if (word == " " && word == Environment.NewLine)
-                {
-                    word = string.Empty;
-                    continue;
-                }
 
                 // Si el lexema actual + el siguiente carácter pueden formar todavia distintos patrones, se sigue leyendo
-                if (i + 1 < _input.Length && _input[i + 1] != ' ')
+                if (i + 1 < _input.Length)
                 {
                     tempWord = word + _input[i + 1];
-                    if (_patternRepo.MatchToken(tempWord).Valid)
+                    if (_input[i + 1].ToString() != Environment.NewLine && _patternRepo.MatchToken(tempWord).Valid)
                         continue;
                 }
 
                 // Si no existen más posibilidades de tokens:
-                Tokens.Add(_patternRepo.MatchToken(word).Token);
+                Tokens.Add(_patternRepo.MatchToken(word.Trim()).Token);
                 word = string.Empty;
             }
         }
