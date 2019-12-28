882e8d0d3c8ac75b34221303b761e05afb0b1c0b
andre close <andreclose@gmail.com>
2019-12-27 21:55:08 -0400

    more cg

--- Magic Compiler Core/Matlab/cg_SeqId.cs
+++ Magic Compiler Core/Matlab/cg_SeqId.cs
@@ -7,72 +7,45 @@ using System.Text;
 
 namespace MagicCompiler.MatLab
 {
-    public class cg_SeqId : ICodeGenerator
+    public class cg_SeqID : ICodeGenerator
     {
         public string[] Productions => new string[]
         {
-            "iniseqid ::= id ,",
-            "seqid ::= id , seqid",
-            "seqid ::= id"
-            
-
+          "seqid ::= seqid , id",
+          "seqid ::= iniseqid id",
+          "seqid ::= id",
+          "iniseqid ::= id ,"
+          
+          
         };
 
         public bool ValidProduction(Production production) => Productions.Contains(production.ToString());
 
         public string Translate(List<Token> tokens, Production production)
         {
-
-            if (production.ToString() == "iniseqid ::= id ,")
+            string prod = production.ToString();
+            string res = string.Empty;
+            if (prod == "iniseqid ::= id ,")
             {
-                string res = "";
-                int firstSymbolIndex = tokens.FindLastIndex(token => token.IsSymbol(Context.symbol_id));
-
-
-                for (int i = firstSymbolIndex; i < tokens.Count; i++)
-                {
-                    if (tokens[i].IsSymbol(Context.symbol_id))
-                    {
-                        res += tokens[i].Lexeme;
-                    }
-                    else if (tokens[i].IsSymbol(Context.symbol_comma))
-                    {
-                        res += tokens[i].Lexeme;
-
-                    }
-
-                }
-                Context.Instance.Translations.Enqueue(res);
-                return res;
+                res = string.Format("{0},", Context.Instance.Translations.Pop());
+                Context.Instance.Translations.Push(res);
             }
-
-            if (production.ToString() == "seqid ::= iniseqid  seqid")
+            else if (prod == "seqid ::= id")
             {
-                string res = Context.Instance.Translations.Dequeue();
-                Context.Instance.Translations.Enqueue(res);
-                return res;
+                res = string.Format("{0} ", Context.Instance.Translations.Pop());
+                Context.Instance.Translations.Push(res);
             }
-
-            if(production.ToString() == "seqid ::= id")
+            else if (prod == "seqid ::= iniseqid id")
             {
-                string res = "";
-                int firstSymbolIndex = tokens.FindLastIndex(token => token.IsSymbol(Context.symbol_id));
-
-                for (int i = firstSymbolIndex; i < tokens.Count; i++)
-                {
-                    if (tokens[i].IsSymbol(Context.symbol_id))
-                    {
-                        res +=tokens[i].Lexeme + ", ";
-                    }
-                    
-
-                }
-                Context.Instance.Translations.Enqueue(res);
-                return res;
+                res = string.Format("{1}{0}", Context.Instance.Translations.Pop(), Context.Instance.Translations.Pop());
+                Context.Instance.Translations.Push(res);
             }
-            return "fdf";
+            else if (prod == "seqid ::= seqid , id")
+            {
+                res = string.Format("{1}, {0}", Context.Instance.Translations.Pop(), Context.Instance.Translations.Pop());
+                Context.Instance.Translations.Push(res);
+            }
+            return res;
         }
-
-
     }
 }
